a246bc660ae332256cb5212e41160986fff21fdc
Elza-Espina <elzaespina@gmail.com>
2017-03-22 16:59:05 +0800

    DIS IZ IT BESHIE

---
+++ src/java/ServerY.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.*;
+import java.util.regex.*;
+
+
+public class ServerY{
+
+public static Double start(){
+return 10.0;
+}
+
+
+}//End of Main class
